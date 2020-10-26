e90d1bc65282092b09a2a1250f66c8634feb8f2b
jzhowie <jzhou20@stuy.edu>
2020-10-26 19:42:25 -0400

    Created BankAccount class and the corresponding constructor

---
+++ BankAccount.java
@@ -0,0 +1,11 @@
+public class BankAccount {
+private double balance;
+private int accountID;
+private String password;
+
+public BankAccount(int ID, String pass){
+	balance = 0;
+	accountID = ID;
+	password = pass;
+}
+}
