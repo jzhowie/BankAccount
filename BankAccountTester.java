45c06f8eb7c6975078687791fd3edf64a8d6dd7d
jzhowie <jzhou20@stuy.edu>
2020-10-26 19:51:09 -0400

    Added test cases for getBalance and getAccountID methods

---
+++ BankAccountTester.java
@@ -0,0 +1,14 @@
+public class BankAccountTester {
+public static void main(String[] args) {
+	BankAccount acc1 = new BankAccount(1418350, "five");
+	BankAccount acc2 = new BankAccount(-619468, "one");
+
+	System.out.println("Balance of accounts (should be zero!)");
+	System.out.println(acc1.getBalance());
+	System.out.println(acc2.getBalance());
+
+	System.out.println("Account IDs, refer to acc1 and acc2");
+	System.out.println(acc1.getAccountID());
+	System.out.println(acc2.getAccountID());
+}
+}
